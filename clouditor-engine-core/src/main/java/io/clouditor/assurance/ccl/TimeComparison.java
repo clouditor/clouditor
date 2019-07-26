be5f0c7350d693401c2923131c73830d4e2cb206
Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2019-07-26 15:48:05 +0200

    fixed a bug in TimeComparison; introduced new time operator

--- clouditor-engine-core/src/main/java/io/clouditor/assurance/ccl/TimeComparison.java
+++ clouditor-engine-core/src/main/java/io/clouditor/assurance/ccl/TimeComparison.java
@@ -29,6 +29,7 @@
 
 package io.clouditor.assurance.ccl;
 
+import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
@@ -52,22 +53,25 @@ public class TimeComparison extends Comparison {
 
     if (fieldValue instanceof Long) {
       instant = Instant.ofEpochSecond((Long) fieldValue);
-    } else {
+    } else if ((fieldValue instanceof Map) && ((Map) fieldValue).get("epochSecond") != null) {
       // field values are not really instants but serialized Instant in the form of epochSecond
       // and nano, we need to re-create the Instant
-      if (!(fieldValue instanceof Map) || ((Map) fieldValue).get("epochSecond") == null) {
-        return false;
-      }
 
       instant =
           Instant.ofEpochSecond(
               ((Number) ((Map) fieldValue).get("epochSecond")).longValue(),
               ((Number) ((Map) fieldValue).get("nano")).longValue());
+    } else {
+      try {
+        instant = Instant.parse((String) fieldValue);
+      } catch (ClassCastException e) {
+        return false;
+      }
     }
 
-    var value = Instant.now().plus(relativeValue, timeUnit);
+    var value = Instant.now().minus(relativeValue, timeUnit);
 
-    if (this.timeOperator == TimeOperator.AFTER) {
+    if (this.timeOperator == TimeOperator.BEFORE || this.timeOperator == TimeOperator.YOUNGER) {
       return instant.isAfter(value);
     } else {
       return instant.isBefore(value);
@@ -96,6 +100,8 @@ public class TimeComparison extends Comparison {
 
   public enum TimeOperator {
     BEFORE,
-    AFTER
+    AFTER,
+    YOUNGER,
+    OLDER
   }
 }
