a0c739e2f1ead939f141cbec308f230c1c8de71c
AndHager <andreas.polling@web.de>
2020-10-23 09:11:54 +0200

    Removed experimental

--- clouditor-engine-core/src/main/java/io/clouditor/data_access_layer/Persistence.java
+++ clouditor-engine-core/src/main/java/io/clouditor/data_access_layer/Persistence.java
@@ -6,7 +6,6 @@ import io.clouditor.assurance.ccl.Condition;
 import io.clouditor.assurance.ccl.FilteredAssetType;
 import io.clouditor.auth.User;
 import io.clouditor.data_access_layer.utils.BoomingFunction;
-import io.clouditor.data_access_layer.utils.DBUtils;
 import io.clouditor.discovery.Asset;
 import io.clouditor.discovery.DiscoveryResult;
 import io.clouditor.discovery.Scan;
@@ -22,6 +21,7 @@ import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Persistence implements Closeable {
 
@@ -45,15 +45,14 @@ public class Persistence implements Closeable {
         return factory;
     }
 
-    private <T> Optional<T> exec(final BoomingFunction<Session, T> function) {
+    private <T> Optional<T> exec(final Function<Session, T> function) {
         Objects.requireNonNull(function);
         Optional<T> result = Optional.empty();
         Optional<Transaction> transaction = Optional.empty();
         try (final Session session = getFactory().openSession()) {
             transaction = Optional.of(session.beginTransaction());
             result = Optional.ofNullable(
-                    function.handleSQLException()
-                            .apply(session)
+                    function.apply(session)
             );
             transaction.ifPresent(EntityTransaction::commit);
         } catch (final HibernateException exception) {
@@ -85,8 +84,7 @@ public class Persistence implements Closeable {
     public <T> List<T> listAll(
             final String tableName,
             final Class<T> resultType
-    ) throws SQLException {
-        DBUtils.testName(tableName);
+    )  {
         return exec(
                 session -> session
                         .createQuery("FROM " + tableName, resultType)
@@ -173,32 +171,27 @@ public class Persistence implements Closeable {
             final String tableName,
             final Runnable runnable
     )  {
-        try {
-            final Class<T> type = (Class<T>) sut.getClass();
-            System.out.println(tableName + persistence.listAll(tableName, type));
+        final Class<T> type = (Class<T>) sut.getClass();
+        System.out.println(tableName + persistence.listAll(tableName, type));
 
-            boolean isPresent = persistence.get(type, sutPK).isPresent();
-            System.out.println(tableName + " IS_PRESENT: " + isPresent);
+        boolean isPresent = persistence.get(type, sutPK).isPresent();
+        System.out.println(tableName + " IS_PRESENT: " + isPresent);
 
-            if (!isPresent)
-                persistence.save(sut);
+        if (!isPresent)
+            persistence.save(sut);
 
-            System.out.println(tableName + persistence.listAll(tableName, type));
+        System.out.println(tableName + persistence.listAll(tableName, type));
 
-            var storedValue = persistence.get(type, sutPK);
-            System.out.println(tableName + " STORED_VALUE: " + storedValue);
+        var storedValue = persistence.get(type, sutPK);
+        System.out.println(tableName + " STORED_VALUE: " + storedValue);
 
-            runnable.run();
+        runnable.run();
 
-            storedValue.ifPresent(persistence::delete);
+        storedValue.ifPresent(persistence::delete);
 
-            isPresent = persistence.get(type, sutPK).isPresent();
-            System.out.println(tableName + " IS_PRESENT: " + isPresent);
+        isPresent = persistence.get(type, sutPK).isPresent();
+        System.out.println(tableName + " IS_PRESENT: " + isPresent);
 
-            System.out.println(tableName + persistence.listAll(tableName, type));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new AssertionError();
-        }
+        System.out.println(tableName + persistence.listAll(tableName, type));
     }
 }
