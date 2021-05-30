a5bb68e20fe5fc7396df2ae85c404687f6193be8
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-05-30 20:23:01 +0200

    Added codecov action (#226)
    * Added codecov action
    * Added badge
    * Added tests and slight refactoring

--- service/discovery/discovery.go
+++ service/discovery/discovery.go
@@ -43,16 +43,24 @@ var log *logrus.Entry
 
 //go:generate protoc -I ../../proto -I ../../third_party discovery.proto --go_out=../.. --go-grpc_out=../.. --openapi_out=../../openapi/discovery
 
-// Service is an implementation of the Clouditor Discovery service
+// Service is an implementation of the Clouditor Discovery service.
+// It should not be used directly, but rather the NewService constructor
+// should be used.
 type Service struct {
 	discovery.UnimplementedDiscoveryServer
+
+	resources map[string]voc.IsResource
 }
 
 func init() {
 	log = logrus.WithField("component", "discovery")
 }
 
-var resources map[string]voc.IsResource = make(map[string]voc.IsResource)
+func NewService() *Service {
+	return &Service{
+		resources: make(map[string]voc.IsResource),
+	}
+}
 
 // Start starts discovery
 func (s Service) Start(ctx context.Context, request *discovery.StartDiscoveryRequest) (response *discovery.StartDiscoveryResponse, err error) {
@@ -62,19 +70,23 @@ func (s Service) Start(ctx context.Context, request *discovery.StartDiscoveryReq
 
 	var discoverer discovery.Discoverer = azure.NewAzureStorageDiscovery()
 
+	s.StartDiscovery(discoverer)
+
+	return response, nil
+}
+
+func (s Service) StartDiscovery(discoverer discovery.Discoverer) {
 	list, _ := discoverer.List()
 
 	for _, v := range list {
-		resources[string(v.GetID())] = v
+		s.resources[string(v.GetID())] = v
 	}
-
-	return response, nil
 }
 
 func (s Service) Query(ctx context.Context, request *emptypb.Empty) (response *discovery.QueryResponse, err error) {
 	var r []*structpb.Value
 
-	for _, v := range resources {
+	for _, v := range s.resources {
 		var s structpb.Value
 
 		// this is probably not the fastest approach, but this
---
+++ service/discovery/discovery_test.go
@@ -0,0 +1,60 @@
+package discovery_test
+
+import (
+	"context"
+	"testing"
+
+	"clouditor.io/clouditor/api/discovery"
+	service_discovery "clouditor.io/clouditor/service/discovery"
+	"clouditor.io/clouditor/voc"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+var service *service_discovery.Service
+
+type mockDiscoverer struct {
+}
+
+func (m mockDiscoverer) List() ([]voc.IsResource, error) {
+	return []voc.IsResource{
+		&voc.ObjectStorageResource{
+			StorageResource: voc.StorageResource{
+				Resource: voc.Resource{
+					ID:   "some-id",
+					Name: "some-name",
+				},
+			},
+		},
+	}, nil
+}
+
+func TestQuery(t *testing.T) {
+	var (
+		discoverer discovery.Discoverer
+		response   *discovery.QueryResponse
+		err        error
+	)
+
+	service = service_discovery.NewService()
+
+	// use our mock discoverer
+	discoverer = mockDiscoverer{}
+
+	// discover some resources
+	service.StartDiscovery(discoverer)
+
+	// query them
+	response, err = service.Query(context.Background(), &emptypb.Empty{})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+
+	assert.NotEmpty(t, response.Result.Values)
+
+	m := response.Result.Values[0].GetStructValue().AsMap()
+
+	assert.NotNil(t, m)
+	assert.Equal(t, "some-id", m["id"])
+	assert.Equal(t, "some-name", m["name"])
+}
