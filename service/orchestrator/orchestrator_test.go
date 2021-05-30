e37575da0f8e888c0351038ee2c0f089e20f62de
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-05-30 20:19:22 +0200

    Added tests and slight refactoring

--- service/orchestrator/orchestrator_test.go
+++ service/orchestrator/orchestrator_test.go
@@ -4,16 +4,40 @@ import (
 	"context"
 	"testing"
 
+	"clouditor.io/clouditor/api/assessment"
 	"clouditor.io/clouditor/api/orchestrator"
 	service_orchestrator "clouditor.io/clouditor/service/orchestrator"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListMetrics(t *testing.T) {
-	service := service_orchestrator.Service{}
+var service service_orchestrator.Service
 
-	response, err := service.ListMetrics(context.TODO(), &orchestrator.ListMetricsRequest{})
+func TestListMetrics(t *testing.T) {
+	var (
+		response *orchestrator.ListMetricsResponse
+		err      error
+	)
+
+	response, err = service.ListMetrics(context.TODO(), &orchestrator.ListMetricsRequest{})
 
 	assert.Nil(t, err)
-	assert.Greater(t, len(response.Metrics), 0)
+	assert.NotEmpty(t, response.Metrics)
+}
+
+func TestGetMetric(t *testing.T) {
+	var (
+		request *orchestrator.GetMetricsRequest
+		metric  *assessment.Metric
+		err     error
+	)
+
+	request = &orchestrator.GetMetricsRequest{
+		MetricId: 1,
+	}
+
+	metric, err = service.GetMetric(context.TODO(), request)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, metric)
+	assert.Equal(t, request.MetricId, metric.Id)
 }
