c2c4f88f25b9796546e88429a02417e58662392e
Banse, Christian <christian.banse@aisec.fraunhofer.de>
2021-05-31 11:55:06 +0200

    added some documentation to CCL

--- ccl/ccl.go
+++ ccl/ccl.go
@@ -23,7 +23,23 @@
 //
 // This file is part of Clouditor Community Edition.
 
-// Package ccl contains the the Cloud Compliance Language (CCL)
+// Package ccl contains the the Cloud Compliance Language (CCL). It is a simple domain specific language to model rules
+// that should apply to discovered resources. It has the following syntax:
+// ```ccl
+// <resourceType> has <expression>
+// ```
+//
+// The `<resourceType>` refers to an existing resource type in the vocabulary. See package voc.
+// The `<expression>` must evaluate to a boolean expression, which decide whether the resource is compliant or not
+//
+// Several different expression exists. For example, a simple comparison can be achieved using
+// ```ccl
+// <field> <operatorType> <literalValue>
+// ```
+//
+// In this case, <field> refers to a field in the resource type, defined the vocabulary. See package voc.
+// <operatorType> can either be `==`, `!=`, `<=`, `<`, `>`, `>=`, or the special `contains` keyword.
+// <literalValue> refers to a literal in either a string, integer, float or boolean format.
 package ccl
 
 import (
