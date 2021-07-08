d8daf615609e57ec99584cfa6eb3e65c1e5419ee
Nico Haas <nico.haas@aisec.fraunhofer.de>
2021-07-08 10:13:19 +0200

    Implemented Listing buckets and instances; with tests
    - actually reservations instead of instances until now

--- service/discovery/aws/s3.go
+++ service/discovery/aws/s3.go
@@ -33,70 +33,41 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/sirupsen/logrus"
 	"log"
 )
 
-// awsS3Discovery handles the AWS API requests regarding the S3 service
-type awsS3Discovery struct {
-	client *s3.Client
-	// ToDo: Change type to bucketsType to list of bucket structs
-	buckets       interface{}
-	bucketNames   []string
-	isDiscovering bool
-}
-
-// NewS3Discovery constructs a new awsS3Discovery initializing the s3-client and isDiscovering with true
-// ToDo: cfg as copy (instead of pointer) since we do not want to change it?
-func NewS3Discovery(cfg aws.Config) *awsS3Discovery {
-	return &awsS3Discovery{
-		client:        s3.NewFromConfig(cfg),
-		buckets:       nil,
-		isDiscovering: true,
-	}
-}
-
 // S3ListBucketsAPI is the interface for the List function (used for mock testing)
-// ToDo: Is it a good idea to do so, i.e. integrating test stuff here? It is recommended by the AWS SDK documentation
 type S3ListBucketsAPI interface {
+	// ListBuckets ToDo: What are and do the (general) context interface, the ListBucketsInput and s3.Options parameters
+	// ListBuckets ToDo: Name has to be "ListBuckets" like in s3.client, right?
 	ListBuckets(ctx context.Context,
 		params *s3.ListBucketsInput,
 		optFns ...func(*s3.Options)) (*s3.ListBucketsOutput, error)
 }
 
-// discoverBuckets retrieves all buckets and its configurations needed for assessing
-func (d *awsS3Discovery) discoverBuckets() {
-	// ToDo: Double check that d.client can't be zero due to "private" access with "public" constructor?
-	d.getBuckets(d.client)
-	d.checkEncryption("")
-	d.checkPublicAccessBlockConfiguration("")
-	d.checkBucketReplication("")
-	d.checkLifeCycleConfiguration("")
+func GetS3Client() *s3.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return s3.NewFromConfig(cfg)
 }
 
-// ToDo: Decide if functions are attached to awsS3Discovery or not. It is, e.g., weird that discoverBuckets
-// calls "d.getBuckets(d.client)"
-
-// getBuckets returns all buckets
-func (d *awsS3Discovery) getBuckets(clientApi S3ListBucketsAPI) {
-	logrus.Println("Discovering s3:")
-	resp, err := clientApi.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+func List(clientApi S3ListBucketsAPI) (resp *s3.ListBucketsOutput, err error) {
+	input := &s3.ListBucketsInput{}
+	// ToDo: Why doesnt "err" throw error due to the second declaration with the same name "err"
+	// ToDo: One return line
+	resp, err = clientApi.ListBuckets(context.TODO(), input)
 	if err != nil {
 		log.Fatalf("Error occured while retrieving buckets: %v", err)
 	}
-	logrus.Printf("Retrieved %v buckets.", len(resp.Buckets))
-	d.buckets = resp
-	for _, bucket := range resp.Buckets {
-		d.bucketNames = append(d.bucketNames, *bucket.Name)
-	}
+	log.Println("Retrieved buckets.")
+	return
 
-	// ToDo: Call getBucketObjects and associate the objects with the buckets
 }
 
-// getBucketObjects returns all objects of the given bucket
-// ToDo: Do we need to iterate through single bucket objects or do we only check the general bucket settings?
-// ToDo: "Overload" method s.t. you can list all objects from all buckets or only from a specific set (e.g. 1) of buckets
-func (d *awsS3Discovery) getBucketObjects(myBucket string) *s3.ListObjectsV2Output {
+func GetObjectsOfBucket(myBucket string) *s3.ListObjectsV2Output {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		fmt.Println("Error occurred:", err)
@@ -117,80 +88,4 @@ func (d *awsS3Discovery) getBucketObjects(myBucket string) *s3.ListObjectsV2Outp
 	}
 
 	return output
-}
-
-// checkEncryption gets the bucket's encryption configuration
-func (d *awsS3Discovery) checkEncryption(bucket string) bool {
-	log.Printf("Checking encryption for bucket %v.\n", bucket)
-	input := s3.GetBucketEncryptionInput{
-		Bucket:              aws.String(bucket),
-		ExpectedBucketOwner: nil,
-	}
-	resp, err := d.client.GetBucketEncryption(context.TODO(), &input)
-	if err != nil {
-		logrus.Errorf("Probably no encryption enabled. Error: %v", err)
-		// ToDo: Simply Return good?
-		return false
-	}
-	logrus.Println("Bucket is encrypted.")
-	for i, rule := range resp.ServerSideEncryptionConfiguration.Rules {
-		log.Printf("Rule %v: %v", i, rule.ApplyServerSideEncryptionByDefault)
-	}
-	return true
-}
-
-// checkPublicAccessBlockConfiguration gets the bucket's access configuration
-func (d *awsS3Discovery) checkPublicAccessBlockConfiguration(bucket string) (false bool) {
-	log.Printf("Check if bucket %v has public access...", bucket)
-	input := s3.GetPublicAccessBlockInput{
-		Bucket:              aws.String(bucket),
-		ExpectedBucketOwner: nil,
-	}
-	resp, err := d.client.GetPublicAccessBlock(context.TODO(), &input)
-	if err != nil {
-		logrus.Errorf("Error found: %v", err)
-		return
-	}
-	logrus.Printf("Found: %v", resp.PublicAccessBlockConfiguration)
-
-	configs := resp.PublicAccessBlockConfiguration
-	// ToDo: Currently every configuration has to be unset. Anyway in future, the configs are saved to struct since the
-	// discovery does no assessment
-	if !configs.BlockPublicAcls || !configs.BlockPublicPolicy || !configs.IgnorePublicAcls || !configs.RestrictPublicBuckets {
-		return
-	}
-	return true
-}
-
-// checkBucketReplication gets the bucket's replication configuration
-func (d *awsS3Discovery) checkBucketReplication(bucket string) (false bool) {
-	log.Printf("Check if bucket '%v' is been replicated...", bucket)
-	input := s3.GetBucketReplicationInput{
-		Bucket:              aws.String(bucket),
-		ExpectedBucketOwner: nil,
-	}
-	resp, err := d.client.GetBucketReplication(context.TODO(), &input)
-	if err != nil {
-		logrus.Errorf("Error (probably no replica configuration): %v", err)
-		return
-	}
-	logrus.Println(resp.ReplicationConfiguration)
-	return true
-}
-
-// checkLifeCycleConfiguration gets the bucket's lifecycle configuration
-// ToDo
-func (d *awsS3Discovery) checkLifeCycleConfiguration(bucket string) (false bool) {
-	log.Printf("Check life cycle configuration for bucket '%v'", bucket)
-	input := s3.GetBucketLifecycleConfigurationInput{
-		Bucket:              aws.String(bucket),
-		ExpectedBucketOwner: nil,
-	}
-	resp, err := d.client.GetBucketLifecycleConfiguration(context.TODO(), &input)
-	if err != nil {
-		logrus.Errorf("Error occurred: %v", err)
-		return
-	}
-	logrus.Printf(string(resp.Rules[0].Expiration.Days))
-	return true
 }
