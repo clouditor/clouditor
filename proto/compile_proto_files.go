74a1db42422c333d7118b0272843449910031849
Banse, Christian <christian.banse@aisec.fraunhofer.de>
2021-09-28 11:46:41 +0200

    Fixed proto compile

--- proto/compile_proto_files.go
+++ proto/compile_proto_files.go
@@ -27,8 +27,8 @@
 
 package proto
 
-//go:generate protoc -I ./ -I ../third_party assessment.proto evidence.proto --go_out=../ --go-grpc_out=../ --go_opt=Mevidence.proto=clouditor.io/clouditor/api/evidence --go-grpc_opt=Mevidence.proto=clouditor.io/clouditor/api/evidence --openapi_out=../openapi/assessment
+//go:generate protoc -I ./ -I ../third_party assessment.proto --go_out=../ --go-grpc_out=../ --go_opt=Mevidence.proto=clouditor.io/clouditor/api/evidence --go-grpc_opt=Mevidence.proto=clouditor.io/clouditor/api/evidence --openapi_out=../openapi/assessment
 //go:generate protoc -I ./ -I ../third_party auth.proto --go_out=../ --go-grpc_out=../
 //go:generate protoc -I ./ -I ../third_party discovery.proto --go_out=../ --go-grpc_out=../ --openapi_out=../openapi/discovery
-//go:generate protoc -I ./ -I ../third_party evidence_store.proto evidence.proto --go_out=../ --go-grpc_out=../  --openapi_out=../openapi/evidence
-//go:generate protoc -I ./ -I ../third_party orchestrator.proto metric.proto --go_out=../ --go-grpc_out=../ --go_opt=Mmetric.proto=clouditor.io/clouditor/api/assessment --go-grpc_opt=Mmetric.proto=clouditor.io/clouditor/api/assessment --openapi_out=../openapi/orchestrator
+//go:generate protoc -I ./ -I ../third_party evidence_store.proto --go_out=../ --go-grpc_out=../  --openapi_out=../openapi/evidence
+//go:generate protoc -I ./ -I ../third_party orchestrator.proto --go_out=../ --go-grpc_out=../ --go_opt=Mmetric.proto=clouditor.io/clouditor/api/assessment --go-grpc_opt=Mmetric.proto=clouditor.io/clouditor/api/assessment --openapi_out=../openapi/orchestrator
