c7997f374614b0c219178706a533f747a088a6a0
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-10-08 20:04:04 +0200

    Added CLI test

---
+++ cli/commands/accounts/accounts_test.go
@@ -0,0 +1,117 @@
+// Copyright 2021 Fraunhofer AISEC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//           $$\                           $$\ $$\   $$\
+//           $$ |                          $$ |\__|  $$ |
+//  $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
+// $$  _____|$$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$ |\_$$  _|  $$  __$$\ $$  __$$\
+// $$ /      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |  $$ |    $$ /  $$ |$$ | \__|
+// $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
+// \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
+//  \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
+//
+// This file is part of Clouditor Community Edition.
+
+package accounts_test
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"testing"
+
+	"clouditor.io/clouditor/api/orchestrator"
+	"clouditor.io/clouditor/cli"
+	"clouditor.io/clouditor/cli/commands/accounts"
+	"clouditor.io/clouditor/cli/commands/login"
+	service_auth "clouditor.io/clouditor/service/auth"
+	service_orchestrator "clouditor.io/clouditor/service/orchestrator"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+var sock net.Listener
+var server *grpc.Server
+
+func TestMain(m *testing.M) {
+	var (
+		err error
+		dir string
+	)
+
+	sock, server, err = service_auth.StartDedicatedAuthServer(":0")
+	orchestrator.RegisterOrchestratorServer(server, &service_orchestrator.Service{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer sock.Close()
+	defer server.Stop()
+
+	dir, err = ioutil.TempDir(os.TempDir(), ".clouditor")
+	if err != nil {
+		panic(err)
+	}
+
+	viper.Set("username", "clouditor")
+	viper.Set("password", "clouditor")
+	viper.Set("session-directory", dir)
+
+	cmd := login.NewLoginCommand()
+	err = cmd.RunE(nil, []string{fmt.Sprintf("localhost:%d", sock.Addr().(*net.TCPAddr).Port)})
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestDiscoverAccount(t *testing.T) {
+	var err error
+	var b bytes.Buffer
+
+	cmd := accounts.NewDiscoverAccountsCommand()
+	err = cmd.RunE(nil, []string{"mock"})
+
+	// should be no error
+	assert.Nil(t, err)
+
+	cli.Output = &b
+
+	cmd = accounts.NewListAccountsCommand()
+	err = cmd.RunE(nil, []string{"mock"})
+
+	// should be no error
+	assert.Nil(t, err)
+
+	// should contain our account then
+	var response *orchestrator.ListAccountsResponse = &orchestrator.ListAccountsResponse{}
+
+	err = protojson.Unmarshal(b.Bytes(), response)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.NotEmpty(t, response.Accounts)
+
+	var account = response.Accounts[0]
+
+	assert.NotNil(t, account)
+	assert.Equal(t, "mock", account.AccountType)
+
+}
