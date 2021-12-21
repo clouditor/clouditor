d03a66a1ece000bc14e005f89436dbde099ceea9
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2021-12-21 09:47:30 +0100

    fmt

--- api/evidence/evidence.go
+++ api/evidence/evidence.go
@@ -28,12 +28,12 @@ package evidence
 import "errors"
 
 var (
-	ErrNotValidResource    = errors.New("resource in evidence is missing")
-	ErrResourceNotStruct   = errors.New("resource in evidence is not struct value")
-	ErrResourceNotMap      = errors.New("resource in evidence is not a map")
-	ErrResourceIdMissing   = errors.New("resource in evidence is missing the id field")
-	ErrResourceIdNotString = errors.New("resource id in evidence is not a string")
-	ErrToolIdMissing       = errors.New("tool id in evidence is missing")
+	ErrNotValidResource       = errors.New("resource in evidence is missing")
+	ErrResourceNotStruct      = errors.New("resource in evidence is not struct value")
+	ErrResourceNotMap         = errors.New("resource in evidence is not a map")
+	ErrResourceIdMissing      = errors.New("resource in evidence is missing the id field")
+	ErrResourceIdNotString    = errors.New("resource id in evidence is not a string")
+	ErrToolIdMissing          = errors.New("tool id in evidence is missing")
 	ErrTimestampMissing       = errors.New("timestamp in evidence is missing")
 	ErrResourceIdFieldMissing = errors.New("field id is missing")
 )
