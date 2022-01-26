4247013febaeeff24b2dd3db902d3fc23047e0fe
Nico Haas <nico.haas@aisec.fraunhofer.de>
2022-01-26 17:55:16 +0100

    Add test for commands.go

---
+++ cli/commands/commands_test.go
@@ -0,0 +1,21 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAddCommands(t *testing.T) {
+	cmd := &cobra.Command{}
+	assert.False(t, cmd.HasSubCommands())
+	AddCommands(cmd)
+	assert.True(t, cmd.HasSubCommands())
+	// Check if 'assessment_result' CMD is part of the sub commands
+	for _, v := range cmd.Commands() {
+		if v.Use == "assessment_result" {
+			return
+		}
+	}
+	t.Error("CMD 'assessment_result' is not part of the sub commands but should be")
+}
