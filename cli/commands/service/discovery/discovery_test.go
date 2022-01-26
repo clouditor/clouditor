86c1818db64b6907cc0a2c5ec76419da5098c549
Nico Haas <nico.haas@aisec.fraunhofer.de>
2022-01-26 17:42:46 +0100

    Add tests to discovery service (cli)

---
+++ cli/commands/service/discovery/discovery_test.go
@@ -0,0 +1,223 @@
+// Copyright 2021 Fraunhofer AISEC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//           $$\                           $$\ $$\   $$\
+//           $$ |                          $$ |\__|  $$ |
+//  $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
+// $$  _____|$$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$ |\_$$  _|  $$  __$$\ $$  __$$\
+// $$ /      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |  $$ |    $$ /  $$ |$$ | \__|
+// $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
+// \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
+//  \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
+//
+// This file is part of Clouditor Community Edition.
+
+package discovery
+
+import (
+	"bytes"
+	"clouditor.io/clouditor/api/discovery"
+	"clouditor.io/clouditor/cli"
+	"clouditor.io/clouditor/cli/commands/login"
+	"clouditor.io/clouditor/persistence"
+	service_auth "clouditor.io/clouditor/service/auth"
+	service_discovery "clouditor.io/clouditor/service/discovery"
+	"clouditor.io/clouditor/voc"
+	"fmt"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
+	"io/ioutil"
+	"net"
+	"os"
+	"testing"
+	"time"
+)
+
+var sock net.Listener
+var server *grpc.Server
+
+func TestMain(m *testing.M) {
+	var (
+		err     error
+		dir     string
+		service *service_discovery.Service
+	)
+
+	err = os.Chdir("../../../../")
+	if err != nil {
+		panic(err)
+	}
+
+	err = persistence.InitDB(true, "", 0)
+	if err != nil {
+		panic(err)
+	}
+
+	service = service_discovery.NewService()
+	service.StartDiscovery(mockDiscoverer{testCase: 2})
+
+	sock, server, err = service_auth.StartDedicatedAuthServer(":0")
+	if err != nil {
+		panic(err)
+	}
+	discovery.RegisterDiscoveryServer(server, service)
+
+	defer func(sock net.Listener) {
+		err = sock.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(sock)
+	defer server.Stop()
+
+	dir, err = ioutil.TempDir(os.TempDir(), ".clouditor")
+	if err != nil {
+		panic(err)
+	}
+
+	viper.Set("username", "clouditor")
+	viper.Set("password", "clouditor")
+	viper.Set("session-directory", dir)
+
+	cmd := login.NewLoginCommand()
+	err = cmd.RunE(nil, []string{fmt.Sprintf("localhost:%d", sock.Addr().(*net.TCPAddr).Port)})
+	if err != nil {
+		panic(err)
+	}
+	defer os.Exit(m.Run())
+}
+
+func TestAddCommands(t *testing.T) {
+	cmd := NewDiscoveryCommand()
+
+	// Check if sub commands were added
+	assert.True(t, cmd.HasSubCommands())
+
+	// Check if NewQueryDiscoveryCommand was added
+	for _, v := range cmd.Commands() {
+		if v.Use == "query" {
+			return
+		}
+	}
+	t.Errorf("No list command was added")
+}
+
+// TestNewStartDiscoveryCommand currently tests only the error case
+// For more tests mock stream to assessment
+func TestNewStartDiscoveryCommand(t *testing.T) {
+	var err error
+
+	cmd := NewStartDiscoveryCommand()
+	err = cmd.RunE(nil, []string{})
+	assert.NotNil(t, err)
+
+	var response = &discovery.StartDiscoveryResponse{}
+	assert.NotNil(t, response)
+	assert.False(t, response.Successful)
+
+}
+
+func TestNewQueryDiscoveryCommand(t *testing.T) {
+	var err error
+	var b bytes.Buffer
+
+	cli.Output = &b
+
+	cmd := NewQueryDiscoveryCommand()
+	err = cmd.RunE(nil, []string{})
+	assert.Nil(t, err)
+
+	var response = &discovery.QueryResponse{}
+	err = protojson.Unmarshal(b.Bytes(), response)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.NotEmpty(t, response.Results.Values)
+
+}
+
+// TODO(lebogg): Copied discoverer mocking stuff from discovery tests. Maybe expose it there?
+
+// mockDiscoverer implements Discoverer and mocks the API to cloud resources
+type mockDiscoverer struct {
+	// testCase allows for different implementations for table tests in TestStartDiscovery
+	testCase int
+}
+
+func (mockDiscoverer) Name() string { return "just mocking" }
+
+func (m mockDiscoverer) List() ([]voc.IsCloudResource, error) {
+	switch m.testCase {
+	case 0:
+		return nil, fmt.Errorf("mock error in List()")
+	case 1:
+		return []voc.IsCloudResource{wrongFormattedResource()}, nil
+	case 2:
+		return []voc.IsCloudResource{
+			&voc.ObjectStorage{
+				Storage: &voc.Storage{
+					CloudResource: &voc.CloudResource{
+						ID:   "some-id",
+						Name: "some-name",
+						Type: []string{"ObjectStorage", "Storage", "Resource"},
+					},
+				},
+				HttpEndpoint: &voc.HttpEndpoint{
+					TransportEncryption: &voc.TransportEncryption{
+						Enforced:   false,
+						Enabled:    true,
+						TlsVersion: "TLS1_2",
+					},
+				},
+			},
+		}, nil
+	default:
+		return nil, nil
+	}
+}
+
+func wrongFormattedResource() voc.IsCloudResource {
+	res1 := mockIsCloudResource{Another: nil}
+	res2 := mockIsCloudResource{Another: &res1}
+	res1.Another = &res2
+	return res1
+}
+
+// mockIsCloudResource implements mockIsCloudResource interface.
+// It is used for json.marshal to fail since it contains circular dependency
+type mockIsCloudResource struct {
+	Another *mockIsCloudResource `json:"Another"`
+}
+
+func (mockIsCloudResource) GetID() voc.ResourceID {
+	return "MockResourceId"
+}
+
+func (mockIsCloudResource) GetName() string {
+	return ""
+}
+
+func (mockIsCloudResource) GetType() []string {
+	return nil
+}
+
+func (mockIsCloudResource) HasType(_ string) bool {
+	return false
+}
+
+func (mockIsCloudResource) GetCreationTime() *time.Time {
+	return nil
+}
