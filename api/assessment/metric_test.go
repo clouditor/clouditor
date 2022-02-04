00a97526d498b6b9f41001b89eb92012e5c9c4e4
Christian Banse <christian.banse@aisec.fraunhofer.de>
2022-02-04 11:54:06 +0100

    Implementing write access to metric database via REST API (#479)
    Co-authored-by: Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
    Co-authored-by: Nico Haas <nico.haas@aisec.fraunhofer.de>

---
+++ api/assessment/metric_test.go
@@ -0,0 +1,80 @@
+package assessment
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMetric_Validate(t *testing.T) {
+	type fields struct {
+		metric *Metric
+	}
+	type args struct {
+		opts []MetricValidationOption
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "Metric is nil",
+			fields: fields{
+				metric: nil,
+			},
+			args: args{opts: []MetricValidationOption{
+				WithMetricRequiresId(),
+			}},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.ErrorIs(t, ErrMetricEmpty, err)
+			},
+		},
+		{
+			name: "Metric ID is empty",
+			fields: fields{
+				metric: &Metric{Id: ""},
+			},
+			args: args{opts: []MetricValidationOption{
+				WithMetricRequiresId(),
+			}},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.ErrorIs(t, ErrMetricIdMissing, err)
+			},
+		},
+		{
+			name: "Metric Name is empty",
+			fields: fields{
+				metric: &Metric{Id: "123"},
+			},
+			args: args{opts: []MetricValidationOption{
+				WithMetricRequiresId(),
+			}},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.ErrorIs(t, ErrMetricNameMissing, err)
+			},
+		},
+		{
+			name: "Successful Validation",
+			fields: fields{
+				metric: &Metric{
+					Id:   "SomeId",
+					Name: "SomeName1",
+				},
+			},
+			args: args{opts: []MetricValidationOption{
+				WithMetricRequiresId(),
+			}},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.Nil(t, err)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := tt.fields.metric
+			tt.wantErr(t, m.Validate(tt.args.opts...), fmt.Sprintf("Validate(%v)", tt.args.opts))
+		})
+	}
+}
