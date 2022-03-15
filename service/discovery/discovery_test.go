bafe6c60d536b980ab1eac4b7bb41dc6784b55f9
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2022-03-15 12:47:51 +0100

    Update query test

--- service/discovery/discovery_test.go
+++ service/discovery/discovery_test.go
@@ -179,6 +179,7 @@ func TestStartDiscovery(t *testing.T) {
 
 func TestQuery(t *testing.T) {
 	s := NewService()
+	s.assessmentStream = &mockAssessmentStream{}
 	s.StartDiscovery(mockDiscoverer{testCase: 2})
 
 	type fields struct {
