03c8b46b92698f621962a81461badb2faf3ec8f6
Banse, Christian <christian.banse@aisec.fraunhofer.de>
2022-03-23 20:13:06 +0100

    streams

---
+++ service/streams.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"errors"
+	"io"
+
+	"google.golang.org/grpc"
+)
+
+// StreamRequester is an interface that can be implemented by a struct
+// to wrap it into a suitable message to be transported into stream
+type StreamRequester[T any] interface {
+	StreamRequest() *T
+	GetId() string
+}
+
+// StreamSendLoop takes care of reading from a channel and sends the content of the
+// channel into a grpc.ClientStream. The main use case is to consolidate the sending
+// of messages into a gRPC into one goroutine, because sending from different goroutines
+// is not allowed.
+func StreamSendLoop[T StreamRequester[S], S any](channel chan T, stream grpc.ClientStream, typ string, target string) {
+	for msg := range channel {
+		err := stream.SendMsg(msg.StreamRequest())
+		if errors.Is(err, io.EOF) {
+			log.Infof("Stream to %s was closed", target)
+			break
+		}
+
+		if err != nil {
+			log.Errorf("Error when sending %s to %s: %v", err, typ, target)
+			break
+		}
+
+		log.Debugf("%s (%v) sent to %s", typ, msg.GetId(), target)
+	}
+}
