227679c921c02f6f8455ca14823537452edf8740
Nico Haas <nico.haas@aisec.fraunhofer.de>
2022-04-19 09:44:24 +0200

    Fix OpenAPI `body` option (no wildcard)

--- api/orchestrator/orchestrator.pb.gw.go
+++ api/orchestrator/orchestrator.pb.gw.go
@@ -1236,7 +1236,7 @@ func request_Orchestrator_UpdateCertificate_0(ctx context.Context, marshaler run
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Certificate); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -1270,7 +1270,7 @@ func local_request_Orchestrator_UpdateCertificate_0(ctx context.Context, marshal
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Certificate); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
