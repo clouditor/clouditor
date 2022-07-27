6dfa21ebf0018b1273c0a1334b61c8447503a48c
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2022-07-27 10:35:15 +0200

    last minor things

--- service/discovery/azure/azure_test.go
+++ service/discovery/azure/azure_test.go
@@ -39,8 +39,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
-	"github.com/Azure/go-autorest/autorest"
-
 	"github.com/stretchr/testify/assert"
 )
 
@@ -72,12 +70,6 @@ func (c *mockAuthorizer) GetToken(_ context.Context, _ policy.TokenRequestOption
 	var token azcore.AccessToken
 
 	return token, nil
-}
-
-func (mockAuthorizer) WithAuthorization() autorest.PrepareDecorator {
-	return func(p autorest.Preparer) autorest.Preparer {
-		return p
-	}
 }
 
 func createResponse(object map[string]interface{}, statusCode int) (res *http.Response, err error) {
