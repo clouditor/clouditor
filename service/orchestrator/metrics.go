f7da2092b0adac744359a317eb2a84ae7d475c07
Nico Haas <nico.haas@aisec.fraunhofer.de>
2022-08-26 15:12:07 +0200

    Fix

--- service/orchestrator/metrics.go
+++ service/orchestrator/metrics.go
@@ -282,7 +282,6 @@ func (svc *Service) UpdateMetricImplementation(_ context.Context, req *orchestra
 }
 
 // ListMetrics lists all available metrics.
-// TODO(leboo): Scope
 func (svc *Service) ListMetrics(_ context.Context, req *orchestrator.ListMetricsRequest) (res *orchestrator.ListMetricsResponse, err error) {
 	res = new(orchestrator.ListMetricsResponse)
 
@@ -311,6 +310,15 @@ func (svc *Service) ListMetrics(_ context.Context, req *orchestrator.ListMetrics
 // listMetricsForControl lists all metrics associated to a given control.
 func (svc *Service) listMetricsForControl(_ context.Context, req *orchestrator.ListMetricsRequest) (res *orchestrator.ListMetricsResponse, err error) {
 	res = new(orchestrator.ListMetricsResponse)
+
+	// Validate the request
+	if err = api.ValidateListRequest[*assessment.Metric](req); err != nil {
+		err = fmt.Errorf("invalid request: %w", err)
+		log.Error(err)
+		err = status.Errorf(codes.InvalidArgument, "%v", err)
+		return
+	}
+
 	control := new(orchestrator.Control)
 
 	// first, get the control
