2218e0aee9f35b33d0f4f424ac03230a6e2675ae
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2022-11-11 13:31:21 +0100

    Add toe validate method

--- api/orchestrator/orchestrator_test.go
+++ api/orchestrator/orchestrator_test.go
@@ -30,6 +30,7 @@ import (
 
 	"clouditor.io/clouditor/api"
 	"clouditor.io/clouditor/api/assessment"
+	"clouditor.io/clouditor/internal/defaults"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/types/known/structpb"
 )
@@ -338,6 +339,78 @@ func TestGetMetricConfigurationRequest_Validate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			req := tt.fields.Request
+			tt.wantErr(t, req.Validate())
+		})
+	}
+}
+
+func TestTargetOfEvaluation_Validate(t *testing.T) {
+	type fields struct {
+		toe *TargetOfEvaluation
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name:   "ToE missing",
+			fields: fields{},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.ErrorContains(t, err, ErrToEIsMissing.Error())
+			},
+		},
+		{
+			name: "AssuranceLevel missing",
+			fields: fields{
+				toe: &TargetOfEvaluation{
+					CloudServiceId: defaults.DefaultTargetCloudServiceID,
+					CatalogId:      defaults.DefaultCatalogID,
+				},
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.ErrorContains(t, err, ErrAssuranceLevelIsMissing.Error())
+			},
+		},
+		{
+			name: "CatalogId missing",
+			fields: fields{
+				toe: &TargetOfEvaluation{
+					CloudServiceId: defaults.DefaultTargetCloudServiceID,
+					AssuranceLevel: &defaults.AssuranceLevelHigh,
+				},
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.ErrorContains(t, err, ErrCatalogIDIsMissing.Error())
+			},
+		},
+		{
+			name: "CloudServiceId is missing",
+			fields: fields{
+				toe: &TargetOfEvaluation{
+					CatalogId:      defaults.DefaultCatalogID,
+					AssuranceLevel: &defaults.AssuranceLevelHigh,
+				},
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.ErrorContains(t, err, assessment.ErrCloudServiceIDIsMissing.Error())
+			},
+		},
+		{
+			name: "Happy path",
+			fields: fields{
+				toe: &TargetOfEvaluation{
+					CloudServiceId: defaults.DefaultTargetCloudServiceID,
+					CatalogId:      defaults.DefaultCatalogID,
+					AssuranceLevel: &defaults.AssuranceLevelHigh,
+				},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := tt.fields.toe
 			tt.wantErr(t, req.Validate())
 		})
 	}
