5be5d7c52e86705a2ae3393bc912fb9450c55b03
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2022-11-28 21:54:30 +0100

    Add some tests

--- internal/util/deref_test.go
+++ internal/util/deref_test.go
@@ -28,13 +28,15 @@ package util
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"clouditor.io/clouditor/voc"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDeref(t *testing.T) {
-	testValue := "testString"
+	var testValue string
+	assert.Equal(t, testValue, Deref(&testValue))
+
+	testValue = "testString"
 	assert.Equal(t, testValue, Deref(&testValue))
 
 	var testInt32 int32 = 12
@@ -59,4 +61,32 @@ func TestDeref(t *testing.T) {
 
 	testByteArray := []byte("testByteArray")
 	assert.Equal(t, testByteArray, Deref(&testByteArray))
+}
+
+func TestRef(t *testing.T) {
+	testValue := "testString"
+	assert.Equal(t, &testValue, Ref(testValue))
+
+	var testInt32 int32 = 12
+	assert.Equal(t, &testInt32, Ref(testInt32))
+
+	var testInt64 int64 = 12
+	assert.Equal(t, &testInt64, Ref(testInt64))
+
+	var testFloat32 float32 = 1.5
+	assert.Equal(t, &testFloat32, Ref(testFloat32))
+
+	var testFloat64 float32 = 1.5
+	assert.Equal(t, &testFloat64, Ref(testFloat64))
+
+	var testBool = true
+	assert.Equal(t, &testBool, Ref(testBool))
+
+	testStruct := voc.GeoLocation{
+		Region: "testlocation",
+	}
+	assert.Equal(t, &testStruct, Ref(testStruct))
+
+	testByteArray := []byte("testByteArray")
+	assert.Equal(t, &testByteArray, Ref(testByteArray))
 }
