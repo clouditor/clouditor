3041b4ac28a04639e5e032663d8d418371ba5b4b
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2022-12-12 11:03:11 +0100

    Fix review comments

--- policies/rego.go
+++ policies/rego.go
@@ -220,12 +220,10 @@ func (re *regoEval) evalMap(baseDir string, serviceID, metricID string, m map[st
 		operators := fmt.Sprintf("%s/policies/operators.rego", baseDir)
 
 		c := map[string]interface{}{
-			"target_value":     config.TargetValue.AsInterface(),
-			"operator":         config.Operator,
-			"metric_id":        metricID,
-			"updated_at":       config.UpdatedAt,
-			"is_default":       config.IsDefault,
-			"cloud_service_id": serviceID,
+			"target_value": config.TargetValue.AsInterface(),
+			"operator":     config.Operator,
+			"updated_at":   config.UpdatedAt,
+			"is_default":   config.IsDefault,
 		}
 
 		store := inmem.NewFromObject(c)
