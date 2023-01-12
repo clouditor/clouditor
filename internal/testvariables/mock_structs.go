1e127ebc24dce415d3e702fd4d835c4027e0dfce
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-01-12 17:15:03 +0100

    Add missing file

---
+++ internal/testvariables/mock_structs.go
@@ -0,0 +1,21 @@
+package testvariables
+
+import "clouditor.io/clouditor/api/auth"
+
+func NewUser1() *auth.User {
+	return &auth.User{
+		Username: "SomeName",
+		Password: "SomePassword",
+		Email:    "SomeMail",
+		FullName: "SomeFullName",
+	}
+}
+
+func NewUser2() *auth.User {
+	return &auth.User{
+		Username: "SomeName2",
+		Password: "SomePassword2",
+		Email:    "SomeMail2",
+		FullName: "SomeFullName2",
+	}
+}
