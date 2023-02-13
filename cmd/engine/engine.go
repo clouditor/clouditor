6fadbfa7ebf26bae12d7079b2d8f3688d507783e
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-02-13 11:06:01 +0100

    Add metadata endpoint (#954)
    Co-authored-by: Christian Banse <oxisto@clouditor.io>

--- cmd/engine/engine.go
+++ cmd/engine/engine.go
@@ -203,9 +203,9 @@ func initConfig() {
 }
 
 func doCmd(_ *cobra.Command, _ []string) (err error) {
-	log.Info("Welcome to new Clouditor 2.0")
+	var rt, _ = service.GetRuntimeInfo()
 
-	fmt.Println(`
+	fmt.Printf(`
            $$\                           $$\ $$\   $$\
            $$ |                          $$ |\__|  $$ |
   $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
@@ -214,7 +214,10 @@ func doCmd(_ *cobra.Command, _ []string) (err error) {
  $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
  \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
   \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
- `)
+ 
+  Version %s
+  `, rt.VersionString())
+	fmt.Println()
 
 	if viper.GetBool(DBInMemoryFlag) {
 		db, err = inmemory.NewStorage()
