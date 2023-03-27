ad92be33fd5f3b30cb919c5e8e67f846b6a35187
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-03-27 08:59:51 +0200

    Delete hardcoded catalog path

--- cmd/engine/engine.go
+++ cmd/engine/engine.go
@@ -265,7 +265,7 @@ func doCmd(_ *cobra.Command, _ []string) (err error) {
 			}),
 	)
 
-	orchestratorService = service_orchestrator.NewService(service_orchestrator.WithStorage(db), service_orchestrator.WithCatalogsFile("eucs_catalogs.json"))
+	orchestratorService = service_orchestrator.NewService(service_orchestrator.WithStorage(db))
 
 	assessmentService = service_assessment.NewService(
 		service_assessment.WithOAuth2Authorizer(
