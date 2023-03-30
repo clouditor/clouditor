2842fa23145ef9456b6b154a0aec581a2da39f76
Christian Banse <christian.banse@aisec.fraunhofer.de>
2023-03-30 20:27:42 +0200

    Added a test function to create a mocked `AuthorizationStrategy`
    This fixes #1038. It also tries to restructure the testfolders a little bit.

---
+++ internal/testutil/servicetest/authz.go
@@ -0,0 +1,40 @@
+package servicetest
+
+import (
+	"context"
+
+	"clouditor.io/clouditor/internal/api"
+	"clouditor.io/clouditor/service"
+	"golang.org/x/exp/slices"
+)
+
+func NewAuthorizationStrategy(all bool, cloudServiceIDs []string) service.AuthorizationStrategy {
+	return &AuthorizationStrategyMock{
+		all:             all,
+		cloudServiceIDs: cloudServiceIDs,
+	}
+}
+
+type AuthorizationStrategyMock struct {
+	all             bool
+	cloudServiceIDs []string
+}
+
+func (a *AuthorizationStrategyMock) CheckAccess(ctx context.Context, _ service.RequestType, req api.CloudServiceRequest) bool {
+	var (
+		list []string
+		all  bool
+	)
+
+	all, list = a.AllowedCloudServices(ctx)
+
+	if all {
+		return true
+	}
+
+	return slices.Contains(list, req.GetCloudServiceId())
+}
+
+func (s *AuthorizationStrategyMock) AllowedCloudServices(_ context.Context) (all bool, IDs []string) {
+	return false, s.cloudServiceIDs
+}
