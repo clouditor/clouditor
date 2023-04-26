a45e27672928a055dfa83009db964a3b404582c2
Christian Banse <oxisto@clouditor.io>
2023-04-26 21:27:20 +0200

    Adding gRPC opts to `StartGRPCServer`
    This also refactors the server options a little bit

--- server/auth_middleware.go
+++ server/auth_middleware.go
@@ -80,16 +80,16 @@ const DefaultJWKSURL = "http://localhost:8080/v1/auth/certs"
 // WithJWKS is an option to provide a URL that contains a JSON Web Key Set (JWKS). The JWKS will be used
 // to validate tokens coming from RPC clients against public keys contains in the JWKS.
 func WithJWKS(url string) StartGRPCServerOption {
-	return func(srv *Server, ac *AuthConfig) {
-		ac.jwksURL = url
-		ac.useJWKS = true
+	return func(c *config) {
+		c.ac.jwksURL = url
+		c.ac.useJWKS = true
 	}
 }
 
 // WithPublicKey is an option to directly provide a ECDSA public key which is used to verify tokens coming from RPC clients.
 func WithPublicKey(publicKey *ecdsa.PublicKey) StartGRPCServerOption {
-	return func(srv *Server, ac *AuthConfig) {
-		ac.publicKey = publicKey
+	return func(c *config) {
+		c.ac.publicKey = publicKey
 	}
 }
 
