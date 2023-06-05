bbd9d62d04b7875e603afa5d726ab782f8d4db22
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-06-05 14:16:16 +0200

    ++

--- service/orchestrator/catalogs_test.go
+++ service/orchestrator/catalogs_test.go
@@ -514,7 +514,7 @@ func TestService_ListControls(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, listControlsResponse.Controls)
 	assert.NotEmpty(t, listControlsResponse.Controls)
-	// there are 50 default controls
+	// there are 30 default controls
 	// TODO(all): Why are IdSec-02 and IdSec-02.1 missing?
 	assert.Equal(t, 30, len(listControlsResponse.Controls))
 
