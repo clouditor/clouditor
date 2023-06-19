615ba85bc765e1dc7a70e5c7d1bde4d9294e2062
Nico Haas <nico.haas@aisec.fraunhofer.de>
2023-06-19 15:57:27 +0200

    Ignore deepsource gorm issue

--- persistence/gorm/gorm.go
+++ persistence/gorm/gorm.go
@@ -126,9 +126,8 @@ func NewStorage(opts ...StorageOption) (s persistence.Storage, err error) {
 	log.Println("Creating storage")
 	// Create storage with default gorm config
 	g := &storage{
-		config: gorm.Config{
-			Logger:                 logger.Default.LogMode(logger.Silent),
-			SkipDefaultTransaction: false,
+		config: gorm.Config{ // skipqc: GO-W1004
+			Logger: logger.Default.LogMode(logger.Silent),
 		},
 		types: DefaultTypes,
 	}
