dd5b4f455000fbefdd621925d68e7b9077165670
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-09-14 09:33:45 +0200

    Add missing files

---
+++ internal/util/slice.go
@@ -0,0 +1,37 @@
+// Copyright 2023 Fraunhofer AISEC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//           $$\                           $$\ $$\   $$\
+//           $$ |                          $$ |\__|  $$ |
+//  $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
+// $$  _____|$$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$ |\_$$  _|  $$  __$$\ $$  __$$\
+// $$ /      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |  $$ |    $$ /  $$ |$$ | \__|
+// $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
+// \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
+//  \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
+//
+// This file is part of Clouditor Community Edition.
+
+package util
+
+// StringToAnySlice convert a string slice to an any slice in the format '[]any{[]any{}}'. That is needed for SQL queries, because we need it in the form '("11111111-1111-1111-1111-111111111111"),("22222222-2222-2222-2222-222222222222")' and not in the form '("11111111-1111-1111-1111-111111111111","22222222-2222-2222-2222-222222222222")'.
+func StringToAnySlice(i []string) []any {
+	var r = []any{}
+
+	for j := range i {
+		r = append(r, []any{i[j]})
+	}
+
+	return r
+}
