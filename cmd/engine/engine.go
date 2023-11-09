324e9722fe66420fb8fb7f55c98a24960ec61caf
Christian Banse <oxisto@clouditor.io>
2023-11-09 23:41:47 +0100

    Allow to specify log level on args

--- cmd/engine/engine.go
+++ cmd/engine/engine.go
@@ -87,6 +87,7 @@ const (
 	DiscoveryProviderFlag            = "discovery-provider"
 	DiscoveryResourceGroupFlag       = "discovery-resource-group"
 	DashboardURLFlag                 = "dashboard-url"
+	LogLevelFlag                     = "log-level"
 
 	DefaultAPIDefaultUser                      = "clouditor"
 	DefaultAPIDefaultPassword                  = "clouditor"
@@ -106,6 +107,7 @@ const (
 	DefaultDiscoveryAutoStart                  = false
 	DefaultDiscoveryResourceGroup              = ""
 	DefaultDashboardURL                        = "http://localhost:8080"
+	DefaultLogLevel                            = "info"
 
 	EnvPrefix = "CLOUDITOR"
 )
@@ -162,6 +164,7 @@ func init() {
 	engineCmd.Flags().StringSliceP(DiscoveryProviderFlag, "p", []string{}, "Providers to discover, separated by comma")
 	engineCmd.Flags().String(DiscoveryResourceGroupFlag, DefaultDiscoveryResourceGroup, "Limit the scope of the discovery to a resource group (currently only used in the Azure discoverer")
 	engineCmd.Flags().String(DashboardURLFlag, DefaultDashboardURL, "The URL of the Clouditor Dashboard. If the embedded server is used, a public OAuth 2.0 client based on this URL will be added")
+	engineCmd.Flags().String(LogLevelFlag, DefaultLogLevel, "The default log level")
 
 	_ = viper.BindPFlag(APIDefaultUserFlag, engineCmd.Flags().Lookup(APIDefaultUserFlag))
 	_ = viper.BindPFlag(APIDefaultPasswordFlag, engineCmd.Flags().Lookup(APIDefaultPasswordFlag))
@@ -190,6 +193,7 @@ func init() {
 	_ = viper.BindPFlag(DiscoveryProviderFlag, engineCmd.Flags().Lookup(DiscoveryProviderFlag))
 	_ = viper.BindPFlag(DiscoveryResourceGroupFlag, engineCmd.Flags().Lookup(DiscoveryResourceGroupFlag))
 	_ = viper.BindPFlag(DashboardURLFlag, engineCmd.Flags().Lookup(DashboardURLFlag))
+	_ = viper.BindPFlag(LogLevelFlag, engineCmd.Flags().Lookup(LogLevelFlag))
 }
 
 func initConfig() {
@@ -202,7 +206,10 @@ func initConfig() {
 }
 
 func doCmd(_ *cobra.Command, _ []string) (err error) {
-	var rt, _ = service.GetRuntimeInfo()
+	var (
+		rt, _ = service.GetRuntimeInfo()
+		level logrus.Level
+	)
 
 	fmt.Printf(`
            $$\                           $$\ $$\   $$\
@@ -217,6 +224,12 @@ func doCmd(_ *cobra.Command, _ []string) (err error) {
   Version %s
   `, rt.VersionString())
 	fmt.Println()
+
+	level, err = logrus.ParseLevel(viper.GetString(LogLevelFlag))
+	if err != nil {
+		return err
+	}
+	logrus.SetLevel(level)
 
 	if viper.GetBool(DBInMemoryFlag) {
 		db, err = inmemory.NewStorage()
