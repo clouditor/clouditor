ffe5806c3710fb85eaa4f29e76dc27603a85e432
Christian Banse <oxisto@clouditor.io>
2024-02-07 13:56:15 +0100

    Consistent output

--- api/ontology/example_ontology_test.go
+++ api/ontology/example_ontology_test.go
@@ -1,6 +1,7 @@
 package ontology
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"google.golang.org/protobuf/encoding/protojson"
@@ -26,6 +27,18 @@ func ExampleMarshal() {
 		panic(err)
 	}
 
+	// We need to use regular JSON marshalling on the output to get a consistent result for tests
+	var m map[string]any
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err = json.Marshal(&m)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println(string(b))
-	// Output: {"cloudResource":{"compute":{"virtualMachine":{"id":"my-id", "name":"My VM", "bootLogging":{"enabled":true}}}}}
+	// Output: {"cloudResource":{"compute":{"virtualMachine":{"bootLogging":{"enabled":true},"id":"my-id","name":"My VM"}}}}
 }
