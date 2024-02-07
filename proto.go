f25a3b9e107b0425e30228229e7ae560b97e70fe
Christian Banse <oxisto@clouditor.io>
2024-02-07 09:08:05 +0100

    Re-generate ontology

--- proto.go
+++ proto.go
@@ -32,7 +32,7 @@ package clouditor
 // - header file
 // - root resource name from the owl file
 // - output path for the proto file (optional, default is "api/ontology.proto")
-//go:generate owl2proto internal/input/urn_webprotege_ontology_e4316a28-d966-4499-bd93-6be721055117.owx internal/input/clouditor_header.txt http://graph.clouditor.io/classes/CloudResource api/ontology/ontology.proto
+//go:generate owl2proto internal/input/urn_webprotege_ontology_e4316a28-d966-4499-bd93-6be721055117.owx internal/input/clouditor_header.txt http://graph.clouditor.io/classes/BaseResource api/ontology/ontology.proto
 //go:generate buf format -w
 //go:generate buf generate
 //go:generate buf generate --template buf.gotag.gen.yaml
