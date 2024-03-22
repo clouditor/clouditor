e6c8090cfdc457e118c9e5147370e0b62b831d2b
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2024-03-22 13:11:17 +0100

    Fix orchestrator test and clean up

--- cmd/orchestrator/orchestrator.go
+++ cmd/orchestrator/orchestrator.go
@@ -32,7 +32,7 @@ import (
 	"os"
 	"strings"
 
-	"clouditor.io/clouditor/v2/internal/auth"
+	"clouditor.io/clouditor/v2/internal/config"
 	"clouditor.io/clouditor/v2/logging/formatter"
 	"clouditor.io/clouditor/v2/persistence"
 	"clouditor.io/clouditor/v2/persistence/gorm"
@@ -45,54 +45,6 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-)
-
-const (
-	APIDefaultUserFlag               = "api-default-user"
-	APIDefaultPasswordFlag           = "api-default-password"
-	APIKeyPasswordFlag               = "api-key-password"
-	APIKeyPathFlag                   = "api-key-path"
-	APIKeySaveOnCreateFlag           = "api-key-save-on-create"
-	APIgRPCPortFlag                  = "api-grpc-port"
-	APIHTTPPortFlag                  = "api-http-port"
-	APICORSAllowedOriginsFlags       = "api-cors-allowed-origins"
-	APICORSAllowedHeadersFlags       = "api-cors-allowed-headers"
-	APICORSAllowedMethodsFlags       = "api-cors-allowed-methods"
-	APIJWKSURLFlag                   = "api-jwks-url"
-	APIStartEmbeddedOAuth2ServerFlag = "api-start-embedded-oauth-server"
-	ServiceOAuth2EndpointFlag        = "service-oauth2-token-endpoint"
-	ServiceOAuth2ClientIDFlag        = "service-oauth2-client-id"
-	ServiceOAuth2ClientSecretFlag    = "service-oauth2-client-secret"
-	DBUserNameFlag                   = "db-user-name"
-	DBPasswordFlag                   = "db-password"
-	DBHostFlag                       = "db-host"
-	DBNameFlag                       = "db-name"
-	DBPortFlag                       = "db-port"
-	DBSSLModeFlag                    = "db-ssl-mode"
-	DBInMemoryFlag                   = "db-in-memory"
-	CreateDefaultTarget              = "target-default-create"
-	DashboardURLFlag                 = "dashboard-url"
-	LogLevelFlag                     = "log-level"
-
-	DefaultAPIDefaultUser                      = "clouditor"
-	DefaultAPIDefaultPassword                  = "clouditor"
-	DefaultAPIgRPCPort                  uint16 = 9090
-	DefaultAPIStartEmbeddedOAuth2Server        = true
-	DefaultServiceOAuth2Endpoint               = "http://localhost:8080/v1/auth/token"
-	DefaultServiceOAuth2ClientID               = "clouditor"
-	DefaultServiceOAuth2ClientSecret           = "clouditor"
-	DefaultDBUserName                          = "postgres"
-	DefaultDBPassword                          = "postgres"
-	DefaultDBHost                              = "localhost"
-	DefaultDBName                              = "postgres"
-	DefaultDBPort                       uint16 = 5432
-	DefaultDBSSLMode                           = "disable"
-	DefaultDBInMemory                          = false
-	DefaultCreateDefaultTarget                 = true
-	DefaultDashboardURL                        = "http://localhost:8080"
-	DefaultLogLevel                            = "info"
-
-	EnvPrefix = "CLOUDITOR"
 )
 
 var (
@@ -115,62 +67,12 @@ func init() {
 	log.Logger.Formatter = formatter.CapitalizeFormatter{Formatter: &logrus.TextFormatter{ForceColors: true}}
 	cobra.OnInitialize(initConfig)
 
-	engineCmd.Flags().String(APIDefaultUserFlag, DefaultAPIDefaultUser, "Specifies the default API username")
-	engineCmd.Flags().String(APIDefaultPasswordFlag, DefaultAPIDefaultPassword, "Specifies the default API password")
-	engineCmd.Flags().String(APIKeyPasswordFlag, auth.DefaultApiKeyPassword, "Specifies the password used to proctect the API private key")
-	engineCmd.Flags().String(APIKeyPathFlag, auth.DefaultApiKeyPath, "Specifies the location of the API private key")
-	engineCmd.Flags().Bool(APIKeySaveOnCreateFlag, auth.DefaultApiKeySaveOnCreate, "Specifies whether the API key should be saved on creation. It will only created if the default location is used.")
-	engineCmd.Flags().Uint16(APIgRPCPortFlag, DefaultAPIgRPCPort, "Specifies the port used for the gRPC API")
-	engineCmd.Flags().Uint16(APIHTTPPortFlag, rest.DefaultAPIHTTPPort, "Specifies the port used for the HTTP API")
-	engineCmd.Flags().String(APIJWKSURLFlag, server.DefaultJWKSURL, "Specifies the JWKS URL used to verify authentication tokens in the gRPC and HTTP API")
-	engineCmd.Flags().String(ServiceOAuth2EndpointFlag, DefaultServiceOAuth2Endpoint, "Specifies the OAuth 2.0 token endpoint")
-	engineCmd.Flags().String(ServiceOAuth2ClientIDFlag, DefaultServiceOAuth2ClientID, "Specifies the OAuth 2.0 client ID")
-	engineCmd.Flags().String(ServiceOAuth2ClientSecretFlag, DefaultServiceOAuth2ClientSecret, "Specifies the OAuth 2.0 client secret")
-	engineCmd.Flags().Bool(APIStartEmbeddedOAuth2ServerFlag, DefaultAPIStartEmbeddedOAuth2Server, "Specifies whether the embedded OAuth 2.0 authorization server is started as part of the REST gateway. For production workloads, an external authorization server is recommended.")
-	engineCmd.Flags().StringArray(APICORSAllowedOriginsFlags, rest.DefaultAllowedOrigins, "Specifies the origins allowed in CORS")
-	engineCmd.Flags().StringArray(APICORSAllowedHeadersFlags, rest.DefaultAllowedHeaders, "Specifies the headers allowed in CORS")
-	engineCmd.Flags().StringArray(APICORSAllowedMethodsFlags, rest.DefaultAllowedMethods, "Specifies the methods allowed in CORS")
-	engineCmd.Flags().String(DBUserNameFlag, DefaultDBUserName, "Provides user name of database")
-	engineCmd.Flags().String(DBPasswordFlag, DefaultDBPassword, "Provides password of database")
-	engineCmd.Flags().String(DBHostFlag, DefaultDBHost, "Provides address of database")
-	engineCmd.Flags().String(DBNameFlag, DefaultDBName, "Provides name of database")
-	engineCmd.Flags().Uint16(DBPortFlag, DefaultDBPort, "Provides port for database")
-	engineCmd.Flags().String(DBSSLModeFlag, DefaultDBSSLMode, "The SSL mode for the database")
-	engineCmd.Flags().Bool(DBInMemoryFlag, DefaultDBInMemory, "Uses an in-memory database which is not persisted at all")
-	engineCmd.Flags().Bool(CreateDefaultTarget, DefaultCreateDefaultTarget, "Creates a default target cloud service if it does not exist")
-	engineCmd.Flags().String(DashboardURLFlag, DefaultDashboardURL, "The URL of the Clouditor Dashboard. If the embedded server is used, a public OAuth 2.0 client based on this URL will be added")
-	engineCmd.Flags().String(LogLevelFlag, DefaultLogLevel, "The default log level")
-
-	_ = viper.BindPFlag(APIDefaultUserFlag, engineCmd.Flags().Lookup(APIDefaultUserFlag))
-	_ = viper.BindPFlag(APIDefaultPasswordFlag, engineCmd.Flags().Lookup(APIDefaultPasswordFlag))
-	_ = viper.BindPFlag(APIKeyPasswordFlag, engineCmd.Flags().Lookup(APIKeyPasswordFlag))
-	_ = viper.BindPFlag(APIKeyPathFlag, engineCmd.Flags().Lookup(APIKeyPathFlag))
-	_ = viper.BindPFlag(APIKeySaveOnCreateFlag, engineCmd.Flags().Lookup(APIKeySaveOnCreateFlag))
-	_ = viper.BindPFlag(APIgRPCPortFlag, engineCmd.Flags().Lookup(APIgRPCPortFlag))
-	_ = viper.BindPFlag(APIHTTPPortFlag, engineCmd.Flags().Lookup(APIHTTPPortFlag))
-	_ = viper.BindPFlag(APIJWKSURLFlag, engineCmd.Flags().Lookup(APIJWKSURLFlag))
-	_ = viper.BindPFlag(ServiceOAuth2EndpointFlag, engineCmd.Flags().Lookup(ServiceOAuth2EndpointFlag))
-	_ = viper.BindPFlag(ServiceOAuth2ClientIDFlag, engineCmd.Flags().Lookup(ServiceOAuth2ClientIDFlag))
-	_ = viper.BindPFlag(ServiceOAuth2ClientSecretFlag, engineCmd.Flags().Lookup(ServiceOAuth2ClientSecretFlag))
-	_ = viper.BindPFlag(APIStartEmbeddedOAuth2ServerFlag, engineCmd.Flags().Lookup(APIStartEmbeddedOAuth2ServerFlag))
-	_ = viper.BindPFlag(APICORSAllowedOriginsFlags, engineCmd.Flags().Lookup(APICORSAllowedOriginsFlags))
-	_ = viper.BindPFlag(APICORSAllowedHeadersFlags, engineCmd.Flags().Lookup(APICORSAllowedHeadersFlags))
-	_ = viper.BindPFlag(APICORSAllowedMethodsFlags, engineCmd.Flags().Lookup(APICORSAllowedMethodsFlags))
-	_ = viper.BindPFlag(DBUserNameFlag, engineCmd.Flags().Lookup(DBUserNameFlag))
-	_ = viper.BindPFlag(DBPasswordFlag, engineCmd.Flags().Lookup(DBPasswordFlag))
-	_ = viper.BindPFlag(DBHostFlag, engineCmd.Flags().Lookup(DBHostFlag))
-	_ = viper.BindPFlag(DBNameFlag, engineCmd.Flags().Lookup(DBNameFlag))
-	_ = viper.BindPFlag(DBPortFlag, engineCmd.Flags().Lookup(DBPortFlag))
-	_ = viper.BindPFlag(DBSSLModeFlag, engineCmd.Flags().Lookup(DBSSLModeFlag))
-	_ = viper.BindPFlag(DBInMemoryFlag, engineCmd.Flags().Lookup(DBInMemoryFlag))
-	_ = viper.BindPFlag(CreateDefaultTarget, engineCmd.Flags().Lookup(CreateDefaultTarget))
-	_ = viper.BindPFlag(DashboardURLFlag, engineCmd.Flags().Lookup(DashboardURLFlag))
-	_ = viper.BindPFlag(LogLevelFlag, engineCmd.Flags().Lookup(LogLevelFlag))
+	engineCmd = config.InitCobra(engineCmd)
 }
 
 func initConfig() {
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-	viper.SetEnvPrefix(EnvPrefix)
+	viper.SetEnvPrefix(config.EnvPrefix)
 	viper.SetConfigName("clouditor")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
@@ -193,26 +95,26 @@ func doCmd(_ *cobra.Command, _ []string) (err error) {
  \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
   \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
  
-  Version %s
+  Clouditor Orchestrator Service Version %s
   `, rt.VersionString())
 	fmt.Println()
 
-	level, err = logrus.ParseLevel(viper.GetString(LogLevelFlag))
+	level, err = logrus.ParseLevel(viper.GetString(config.LogLevelFlag))
 	if err != nil {
 		return err
 	}
 	logrus.SetLevel(level)
 
-	if viper.GetBool(DBInMemoryFlag) {
+	if viper.GetBool(config.DBInMemoryFlag) {
 		db, err = inmemory.NewStorage()
 	} else {
 		db, err = gorm.NewStorage(gorm.WithPostgres(
-			viper.GetString(DBHostFlag),
-			viper.GetUint16(DBPortFlag),
-			viper.GetString(DBUserNameFlag),
-			viper.GetString(DBPasswordFlag),
-			viper.GetString(DBNameFlag),
-			viper.GetString(DBSSLModeFlag),
+			viper.GetString(config.DBHostFlag),
+			viper.GetUint16(config.DBPortFlag),
+			viper.GetString(config.DBUserNameFlag),
+			viper.GetString(config.DBPasswordFlag),
+			viper.GetString(config.DBNameFlag),
+			viper.GetString(config.DBSSLModeFlag),
 		))
 	}
 	if err != nil {
@@ -228,21 +130,20 @@ func doCmd(_ *cobra.Command, _ []string) (err error) {
 
 	// orchestratorService.RegisterAssessmentResultHook(func(result *assessment.AssessmentResult, err error) {})
 
-	// TODO(anatheka): Who is creating the default target cloud service?
-	// if viper.GetBool(CreateDefaultTarget) {
-	// 	_, err := orchestratorService.CreateDefaultTargetCloudService()
-	// 	if err != nil {
-	// 		log.Errorf("could not register default target cloud service: %v", err)
-	// 	}
-	// }
+	if viper.GetBool(config.CreateDefaultTarget) {
+		_, err := orchestratorService.CreateDefaultTargetCloudService()
+		if err != nil {
+			log.Errorf("could not register default target cloud service: %v", err)
+		}
+	}
 
-	grpcPort := viper.GetUint16(APIgRPCPortFlag)
-	httpPort := viper.GetUint16(APIHTTPPortFlag)
+	grpcPort := viper.GetUint16(config.APIgRPCPortFlag)
+	httpPort := viper.GetUint16(config.APIHTTPPortFlag)
 
 	var opts = []rest.ServerConfigOption{
-		rest.WithAllowedOrigins(viper.GetStringSlice(APICORSAllowedOriginsFlags)),
-		rest.WithAllowedHeaders(viper.GetStringSlice(APICORSAllowedHeadersFlags)),
-		rest.WithAllowedMethods(viper.GetStringSlice(APICORSAllowedMethodsFlags)),
+		rest.WithAllowedOrigins(viper.GetStringSlice(config.APICORSAllowedOriginsFlags)),
+		rest.WithAllowedHeaders(viper.GetStringSlice(config.APICORSAllowedHeadersFlags)),
+		rest.WithAllowedMethods(viper.GetStringSlice(config.APICORSAllowedMethodsFlags)),
 	}
 
 	log.Infof("Starting gRPC endpoint on :%d", grpcPort)
@@ -250,7 +151,7 @@ func doCmd(_ *cobra.Command, _ []string) (err error) {
 	// Start the gRPC server
 	_, srv, err = server.StartGRPCServer(
 		fmt.Sprintf("0.0.0.0:%d", grpcPort),
-		server.WithJWKS(viper.GetString(APIJWKSURLFlag)),
+		server.WithJWKS(viper.GetString(config.APIJWKSURLFlag)),
 		server.WithOrchestrator(orchestratorService),
 		server.WithReflection(),
 	)
