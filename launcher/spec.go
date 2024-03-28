3aebca7e9218ce9ce0f4b25ac03e49fa8abe51e5
Christian Banse <oxisto@clouditor.io>
2024-03-28 12:46:12 +0100

    Added copyright

--- launcher/spec.go
+++ launcher/spec.go
@@ -1,3 +1,28 @@
+// Copyright 2024 Fraunhofer AISEC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//           $$\                           $$\ $$\   $$\
+//           $$ |                          $$ |\__|  $$ |
+//  $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
+// $$  _____|$$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$ |\_$$  _|  $$  __$$\ $$  __$$\
+// $$ /      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |  $$ |    $$ /  $$ |$$ | \__|
+// $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
+// \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
+//  \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
+//
+// This file is part of Clouditor Community Edition.
+
 package launcher
 
 import (
@@ -6,6 +31,9 @@ import (
 	"clouditor.io/clouditor/v2/service"
 )
 
+// spec is a struct that implements [ServiceSpec]. We want to keep it unexported, because it needs to be generic to the
+// type T, in order for the initialization and creation functions to work. But if we want to have a list of different
+// specs, we cannot mix the generics, therefore we need to have the [ServiceSpec] interface.
 type spec[T service.Service] struct {
 	nsf  NewServiceFunc[T]
 	wsf  WithStorageFunc[T]
