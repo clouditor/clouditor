3b223d54c2d6572cb543246690ba84383ef0a052
Christian Banse <oxisto@clouditor.io>
2024-04-19 22:44:49 +0200

    Made linter happy

--- service/discovery/extra/csaf/discovery_test.go
+++ service/discovery/extra/csaf/discovery_test.go
@@ -1,3 +1,28 @@
+// Copyright 2024 Fraunhofer AISEC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//           $$\                           $$\ $$\   $$\
+//           $$ |                          $$ |\__|  $$ |
+//  $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
+// $$  _____|$$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$ |\_$$  _|  $$  __$$\ $$  __$$\
+// $$ /      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |  $$ |    $$ /  $$ |$$ | \__|
+// $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
+// \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
+//  \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
+//
+// This file is part of Clouditor Community Edition.
+
 // csaf contains a discover that discovery security advisory information from a CSAF trusted provider
 package csaf
 
@@ -35,7 +60,7 @@ func Test_csafDiscovery_List(t *testing.T) {
 		wantList []ontology.IsResource
 		wantErr  assert.WantErr
 	}{
-		/*{
+		{
 			name: "fail",
 			fields: fields{
 				domain: "localhost:1234",
@@ -45,7 +70,7 @@ func Test_csafDiscovery_List(t *testing.T) {
 			wantErr: func(t *testing.T, err error) bool {
 				return assert.ErrorContains(t, err, "could not load provider-metadata.json")
 			},
-		},*/
+		},
 		{
 			name: "happy path",
 			fields: fields{
--- service/discovery/extra/csaf/providertest/providertest.go
+++ service/discovery/extra/csaf/providertest/providertest.go
@@ -1,3 +1,28 @@
+// Copyright 2024 Fraunhofer AISEC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//           $$\                           $$\ $$\   $$\
+//           $$ |                          $$ |\__|  $$ |
+//  $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
+// $$  _____|$$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$ |\_$$  _|  $$  __$$\ $$  __$$\
+// $$ /      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |  $$ |    $$ /  $$ |$$ | \__|
+// $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
+// \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
+//  \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
+//
+// This file is part of Clouditor Community Edition.
+
 package providertest
 
 import (
@@ -33,15 +58,22 @@ func NewTrustedProvider(fns ...func(*csaf.ProviderMetadata)) (p *TrustedProvider
 }
 
 func (p *TrustedProvider) handlePMD(w http.ResponseWriter, r *http.Request) {
-	bytes, err := json.Marshal(p.pmd)
+	writeJSON(w, p.pmd)
+}
+
+func (p *TrustedProvider) Domain() string {
+	return p.Listener.Addr().String()
+}
+
+func writeJSON(w http.ResponseWriter, msg any) {
+	bytes, err := json.Marshal(msg)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	w.Write(bytes)
-}
-
-func (p *TrustedProvider) Domain() string {
-	return p.Listener.Addr().String()
+	_, err = w.Write(bytes)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
