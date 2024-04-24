cec48bf310b93f4b2e3bd3771c9cd98768ee5617
Nico Haas <nico.haas@aisec.fraunhofer.de>
2024-04-24 12:19:21 +0200

    Add error handling

--- internal/testutil/servicetest/discoverytest/csaf/providertest/indextxt.go
+++ internal/testutil/servicetest/discoverytest/csaf/providertest/indextxt.go
@@ -12,6 +12,7 @@ import (
 	"clouditor.io/clouditor/v2/internal/util"
 
 	"github.com/csaf-poc/csaf_distribution/v3/csaf"
+	log "github.com/sirupsen/logrus"
 )
 
 type ServiceHandler interface {
@@ -31,26 +32,34 @@ func NewGoodIndexTxtWriter() ServiceHandler {
 
 type goodIndexTxtWriter struct{}
 
-func (good *goodIndexTxtWriter) handleIndexTxt(w http.ResponseWriter, r *http.Request, advisories []*csaf.Advisory) {
+func (good *goodIndexTxtWriter) handleIndexTxt(_ http.ResponseWriter, _ *http.Request, advisories []*csaf.Advisory) {
 	for _, advisory := range advisories {
 		// write something, take URL from tracking ID
 		_ = advisory.Document.Tracking.ID
 	}
 }
 
-func (good *goodIndexTxtWriter) handleChangesCsv(w http.ResponseWriter, r *http.Request, advisories []*csaf.Advisory) {
+func (good *goodIndexTxtWriter) handleChangesCsv(w http.ResponseWriter, _ *http.Request, advisories []*csaf.Advisory) {
 	// TODO: must be sorted!
 	for _, advisory := range advisories {
 		line := fmt.Sprintf("\"%s\",\"%s\"\n", DocURL(advisory.Document), util.Deref(advisory.Document.Tracking.CurrentReleaseDate))
 		// write something, take release from tracking current_release_data
-		w.Write([]byte(line))
+		_, err := w.Write([]byte(line))
+		// Maybe do better error handling
+		if err != nil {
+			log.Warnf("Could not write csv: %s", err.Error())
+		}
 	}
 }
 
-func (good *goodIndexTxtWriter) handleAdvisory(w http.ResponseWriter, r *http.Request, advisory *csaf.Advisory) {
+func (good *goodIndexTxtWriter) handleAdvisory(w http.ResponseWriter, _ *http.Request, advisory *csaf.Advisory) {
 	b, err := json.Marshal(advisory)
 	if err == nil {
-		w.Write(b)
+		_, err = w.Write(b)
+		// Maybe do better error handling
+		if err != nil {
+			log.Warnf("Could not write: %s", err.Error())
+		}
 	}
 }
 
