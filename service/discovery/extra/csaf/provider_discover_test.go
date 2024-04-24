778935f530faaea825528f27438b4621cbc65a73
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2024-04-24 23:03:55 +0200

    Delete not implemented test

--- service/discovery/extra/csaf/provider_discover_test.go
+++ service/discovery/extra/csaf/provider_discover_test.go
@@ -7,40 +7,6 @@ import (
 	"clouditor.io/clouditor/v2/api/ontology"
 )
 
-// func Test_csafDiscovery_discoverProviders(t *testing.T) {
-// 	type fields struct {
-// 		domain string
-// 		csID   string
-// 		client *http.Client
-// 	}
-// 	type args struct {
-// 	}
-// 	tests := []struct {
-// 		name          string
-// 		fields        fields
-// 		args          args
-// 		wantProviders []ontology.IsResource
-// 		wantErr       bool
-// 	}{}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			d := &csafDiscovery{
-// 				domain: tt.fields.domain,
-// 				csID:   tt.fields.csID,
-// 				client: tt.fields.client,
-// 			}
-// 			gotProviders, err := d.discoverProviders()
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("discoverProviders() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-// 			if !reflect.DeepEqual(gotProviders, tt.wantProviders) {
-// 				t.Errorf("discoverProviders() gotProviders = %v, want %v", gotProviders, tt.wantProviders)
-// 			}
-// 		})
-// 	}
-// }
-
 func Test_getIDsOf(t *testing.T) {
 	type args struct {
 		documents []ontology.IsResource
