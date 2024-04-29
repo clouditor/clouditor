72a5114d966e310d20ac5d18d75ae136bc046f97
Christian Banse <oxisto@clouditor.io>
2024-04-29 10:52:34 +0200

    Added more tests

--- service/discovery/extra/csaf/document_discover_test.go
+++ service/discovery/extra/csaf/document_discover_test.go
@@ -68,3 +68,56 @@ func Test_csafDiscovery_handleAdvisory(t *testing.T) {
 		})
 	}
 }
+
+func Test_csafDiscovery_discoverSecurityAdvisories(t *testing.T) {
+	type fields struct {
+		domain string
+		csID   string
+		client *http.Client
+	}
+	type args struct {
+		md       *csaf.LoadedProviderMetadata
+		keyring  openpgp.EntityList
+		parentId string
+	}
+	tests := []struct {
+		name          string
+		fields        fields
+		args          args
+		wantDocuments assert.Want[[]ontology.IsResource]
+		wantErr       bool
+	}{
+		{
+			name: "happy path",
+			fields: fields{
+				domain: goodProvider.Domain(),
+				csID:   discovery.DefaultCloudServiceID,
+				client: goodProvider.Client(),
+			},
+			args: args{
+				md: &csaf.LoadedProviderMetadata{
+					URL:      goodProvider.WellKnownProviderURL(),
+					Document: goodProvider.DocumentAny(),
+				},
+			},
+			wantDocuments: func(t *testing.T, got []ontology.IsResource) bool {
+				return assert.NotEmpty(t, got) && assert.Equal(t, "some-id", got[0].GetId())
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &csafDiscovery{
+				domain: tt.fields.domain,
+				csID:   tt.fields.csID,
+				client: tt.fields.client,
+			}
+			gotDocuments, err := d.discoverSecurityAdvisories(tt.args.md, tt.args.keyring, tt.args.parentId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("csafDiscovery.discoverSecurityAdvisories() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			tt.wantDocuments(t, gotDocuments)
+		})
+	}
+}
