a2b1c5655306add8e4df46075007e3f83111267c
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2025-01-30 18:45:21 +0100

    Fix review comments

--- service/discovery/openstack/account_discover.go
+++ service/discovery/openstack/account_discover.go
@@ -39,11 +39,11 @@ func (d *openstackDiscovery) discoverDomains() (list []ontology.IsResource, err 
 	var opts domains.ListOptsBuilder = &domains.ListOpts{}
 	list, err = genericList(d, d.identityClient, domains.List, d.handleDomain, domains.ExtractDomains, opts)
 
+	// if we cannot retrieve the domain information by calling the API or from the environment variables, we will add the information manually if we already got the domain ID and/or domain name
 	if err != nil {
-		// if we cannot retrieve the domain information by calling the API or from the environment variables, we will add the information manually if we already got the domain ID and/or domain name
-		log.Debugf("could not discover domains due to insufficient permissions, but we can proceed with less domain information: %v", err)
+		log.Debugf("Could not discover domains due to insufficient permissions, but we can proceed with less domain information: %v", err)
 
-		if d.domain.domainID == "" && d.domain.domainName == "" {
+		if d.domain.domainID == "" {
 			err := fmt.Errorf("neither the domain ID nor the domain name are available: %v", err)
 			return nil, err
 		}
@@ -65,11 +65,11 @@ func (d *openstackDiscovery) discoverProjects() (list []ontology.IsResource, err
 	var opts projects.ListOptsBuilder = &projects.ListOpts{}
 	list, err = genericList(d, d.identityClient, projects.List, d.handleProject, projects.ExtractProjects, opts)
 
+	// if we cannot retrieve the project information by calling the API or from the environment variables, we will add the information manually if we already got the project ID and/or project name
 	if err != nil {
-		// if we cannot retrieve the project information by calling the API or from the environment variables, we will add the information manually if we already got the project ID and/or project name
-		log.Debugf("could not discover projects/tenants due to insufficient permissions, but we can proceed with less project/tenant information: %v", err)
+		log.Debugf("Could not discover projects/tenants due to insufficient permissions, but we can proceed with less project/tenant information: %v", err)
 
-		if d.project.projectID == "" && d.project.projectName == "" {
+		if d.project.projectID == "" {
 			err := fmt.Errorf("neither the project ID nor the project name are available: %v", err)
 			return nil, err
 		}
