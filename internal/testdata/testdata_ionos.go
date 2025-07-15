13417b7a8d3dbb8ce7fcb188d161eaf29ace9090
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2025-07-15 13:38:35 +0200

    clean up ionos testdata

---
+++ internal/testdata/testdata_ionos.go
@@ -0,0 +1,56 @@
+// Copyright 2025 Fraunhofer AISEC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//           $$\                           $$\ $$\   $$\
+//           $$ |                          $$ |\__|  $$ |
+//  $$$$$$$\ $$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$\ $$$$$$\    $$$$$$\   $$$$$$\
+// $$  _____|$$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$ |\_$$  _|  $$  __$$\ $$  __$$\
+// $$ /      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |  $$ |    $$ /  $$ |$$ | \__|
+// $$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |  $$ |$$ |
+// \$$$$$$\  $$ |\$$$$$   |\$$$$$   |\$$$$$$  |$$ |  \$$$   |\$$$$$   |$$ |
+//  \_______|\__| \______/  \______/  \_______|\__|   \____/  \______/ \__|
+//
+// This file is part of Clouditor Community Edition.
+
+package testdata
+
+const (
+	// volume
+	MockIonosVolumeID1   = "39d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosVolumeID2   = "49d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosVolumeName1 = "Mock IONOS Block Storage 1"
+	MockIonosVolumeName2 = "Mock IONOS Block Storage 2"
+
+	// datacenter
+	MockIonosDatacenterID1 = "99d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosDatacenterID2 = "a9d85e98-c3da-11ed-afa1-0242ac120002"
+
+	// server
+	MockIonosVMID1   = "79d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosVMID2   = "89d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosVMName1 = "Mock IONOS VM1"
+	MockIonosVMName2 = "Mock IONOS VM2"
+
+	// load balancer
+	MockIonosLoadBalancerID1   = "b9d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosLoadBalancerName1 = "Mock IONOS Load Balancer 1"
+	MockIonosLoadBalancerID2   = "c9d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosLoadBalancerName2 = "Mock IONOS Load Balancer 2"
+
+	// network
+	MockIonosNicID1   = "59d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosNicID2   = "69d85e98-c3da-11ed-afa1-0242ac120002"
+	MockIonosNicName1 = "Mock IONOS NIC 1"
+	MockIonosNicName2 = "Mock IONOS NIC 2"
+)
