f7288ac658eacd64f388c9752a78dfb01fd9c76a
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-03-05 11:36:14 +0100

    Add OpenStack l3FirewallEnabled and restrictedPorts

--- service/discovery/openstack/network_handle.go
+++ service/discovery/openstack/network_handle.go
@@ -26,16 +26,50 @@
 package openstack
 
 import (
+	"context"
+	"strings"
+
 	"clouditor.io/clouditor/v2/api/discovery"
 	"clouditor.io/clouditor/v2/api/ontology"
 	"clouditor.io/clouditor/v2/internal/util"
 
 	"github.com/gophercloud/gophercloud/v2/openstack/networking/v2/networks"
+	"github.com/gophercloud/gophercloud/v2/openstack/networking/v2/ports"
+	"github.com/gophercloud/gophercloud/v2/pagination"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // handleNetworkInterfaces creates a network interface resource based on the Clouditor Ontology
 func (d *openstackDiscovery) handleNetworkInterfaces(network *networks.Network) (ontology.IsResource, error) {
+	var (
+		l3FirewallEnabled   = false
+		restrictedPortsList = []string{}
+	)
+
+	// Check if any port associated with the network has security groups enabled.
+	// If at least one port has security groups, we consider the L3 firewall to be enabled for the entire network.
+	pager := ports.List(d.clients.networkClient, ports.ListOpts{
+		NetworkID: network.ID,
+	})
+	err := pager.EachPage(context.Background(), func(ctx context.Context, page pagination.Page) (bool, error) {
+		portList, err := ports.ExtractPorts(page)
+		if err != nil {
+			return false, err
+		}
+
+		for _, port := range portList {
+			if len(port.SecurityGroups) > 0 {
+				l3FirewallEnabled = true
+				restrictedPortsList = append(restrictedPortsList, d.getRestrictedPorts(port.SecurityGroups)...)
+				return true, nil
+			}
+		}
+		return true, nil
+	})
+	if err != nil {
+		log.Errorf("Error listing ports for network %s: %s", network.ID, err)
+	}
+
 	r := &ontology.NetworkInterface{
 		Id:           network.ID,
 		Name:         network.Name,
@@ -47,9 +81,22 @@ func (d *openstackDiscovery) handleNetworkInterfaces(network *networks.Network) 
 		Labels:   labels(util.Ref(network.Tags)),
 		ParentId: util.Ref(network.ProjectID),
 		Raw:      discovery.Raw(network),
+		AccessRestriction: &ontology.AccessRestriction{
+			Type: &ontology.AccessRestriction_L3Firewall{
+				L3Firewall: &ontology.L3Firewall{
+					Enabled:         l3FirewallEnabled,
+					RestrictedPorts: strings.Join(restrictedPortsList, ", "),
+				},
+			},
+		},
 	}
 
 	log.Infof("Adding network interface '%s", network.Name)
 
 	return r, nil
+}
+
+func (d *openstackDiscovery) handlePorts(network *networks.Network) (ontology.IsResource, error) {
+
+	return nil, nil
 }
